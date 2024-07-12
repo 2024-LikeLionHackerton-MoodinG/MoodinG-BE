0207df1f2660654a818db854b667e29bfb183ab5
kokodak <kokodakadokok@gmail.com>
2024-07-13 00:00:49 +0900

    mooding project init
    Co-authored-by: whxogus215 <whxogus215@naver.com>

---
+++ src/main/java/com/likelion/mooding/MoodingApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.mooding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/mooding/MoodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.mooding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
