c7ed2ea1088d4590ce3002ed781a3aff550c3c98
Seungyong Lee <kokodakadokok@gmail.com>
2024-07-24 16:39:56 +0900

    feat: 피드백 진행상태 요청 API 구현 (#18)
    Co-authored-by: whxogus215 <whxogus215@naver.com>

---
+++ src/test/java/com/likelion/mooding/e2e/E2EClient.java
@@ -0,0 +1,51 @@
+package com.likelion.mooding.e2e;
+
+import com.likelion.mooding.feedback.presentation.dto.FeedbackCreateRequest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.springframework.http.MediaType;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+public class E2EClient {
+
+    public static ExtractableResponse<Response> 세션_요청() {
+        return RestAssured.given()
+                          .log().all()
+                          .when()
+                          .post("/session")
+                          .then()
+                          .log().all()
+                          .extract();
+    }
+
+    public static ExtractableResponse<Response> 피드백_생성_요청(final String sessionId,
+                                                          final FeedbackCreateRequest request) {
+        return RestAssured.given()
+                          .sessionId(sessionId)
+                          .contentType(MediaType.APPLICATION_JSON_VALUE)
+                          .body(request)
+                          .log().all()
+                          .when()
+                          .post("/feedback")
+                          .then()
+                          .log().all()
+                          .extract();
+    }
+
+    public static ExtractableResponse<Response> 피드백_진행상태_요청(final String sessionId,
+                                                            final Long id) {
+        return RestAssured.given()
+                          .sessionId(sessionId)
+                          .contentType(MediaType.APPLICATION_JSON_VALUE)
+                          .log().all()
+                          .when()
+                          .get("/feedback/status/{id}", id)
+                          .then()
+                          .log().all()
+                          .extract();
+    }
+}
