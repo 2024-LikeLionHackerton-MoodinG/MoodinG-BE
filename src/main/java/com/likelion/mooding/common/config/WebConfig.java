38802c7a5e7be2eea90a84595296869de18a3b5c
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-07-30 15:04:19 +0900

    fix: 3000포트 요청에 대한 CORS 허용 (#27)

--- src/main/java/com/likelion/mooding/common/config/WebConfig.java
+++ src/main/java/com/likelion/mooding/common/config/WebConfig.java
@@ -5,6 +5,7 @@ import com.likelion.mooding.auth.presentation.interceptor.GuestInterceptor;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,5 +21,16 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(new GuestInterceptor())
                 .addPathPatterns("/feedback/**");
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
