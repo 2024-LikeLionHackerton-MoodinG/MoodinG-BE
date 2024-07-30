712c866b42a34701bd13adbfc4a33c236a24d94e
kokodak <kokodakadokok@gmail.com>
2024-07-30 16:42:09 +0900

    fix: EC2 CORS 해결

--- src/main/java/com/likelion/mooding/common/config/WebConfig.java
+++ src/main/java/com/likelion/mooding/common/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.likelion.mooding.common.config;
 import com.likelion.mooding.auth.presentation.argumentresolver.GuestArgumentResolver;
 import com.likelion.mooding.auth.presentation.interceptor.GuestInterceptor;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,6 +12,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final String ALLOWED_ORIGIN;
+
+    public WebConfig(@Value("${allowed-origin}") final String ALLOWED_ORIGIN) {
+        this.ALLOWED_ORIGIN = ALLOWED_ORIGIN;
+    }
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
@@ -26,7 +33,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", ALLOWED_ORIGIN)
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
